9ea069cb266d1636d15d78ce7730646ded917ba3
Rafael Yamada <rafaelyamada321@gmail.com>
2021-04-08 18:43:46 -0300

    bug resolvido - floor para ceil no JobUtils

--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -17,7 +17,7 @@ module.exports = {
 
     //transformar millisegundos em dias
     const dayInMs = 1000 * 60 * 60 * 24;
-    const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+    const dayDiff = Math.ceil(timeDiffInMs / dayInMs);
 
     // restam x dias
     return dayDiff;
